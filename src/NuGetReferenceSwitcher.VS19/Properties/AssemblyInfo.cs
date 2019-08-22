a0538452bbe427073c40a267219212193064065f
Lucas Rossoni <lucas.rossoni@inflor.com.br>
2019-08-22 14:27:32 -0300

    Creating extension version to Visual Studio 2019

---
+++ src/NuGetReferenceSwitcher.VS19/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGetReferenceSwitcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rico Suter")]
+[assembly: AssemblyProduct("NuGetReferenceSwitcher")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: AssemblyVersion("1.9.*")]
